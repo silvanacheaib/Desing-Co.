976d90d4dadc9eb6c32f0d7d461e039bdad70eaf
silvanacheaib <silvanacheaib@gmail.com>
2026-05-07 10:34:46 +0300

    Creating an SPFX Project!

---
+++ Projects/Sharepoint SPFX/sp-branding/src/extensions/onPrimeBranding/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IOnPrimeBrandingApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'OnPrimeBrandingApplicationCustomizerStrings' {
+  const strings: IOnPrimeBrandingApplicationCustomizerStrings;
+  export = strings;
+}
